d6ab01175bbb59ac8b0acdd9e54c29147fafd1d1
Brian Ronald <brian@ppcis.org>
2015-03-03 21:12:55 +0000

    Remove an additional, unnecessary path separator

--- src/main/java/org/ppcis/ccistool/Gui.java
+++ src/main/java/org/ppcis/ccistool/Gui.java
@@ -7,7 +7,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -127,7 +126,7 @@ public class Gui implements ActionListener {
         FileDialog fileDialog = new FileDialog(jFrame,"FileOpen",FileDialog.LOAD);
         fileDialog.setFile(fileSpec);
         fileDialog.setVisible(true);
-        return String.format("%s%s%s", fileDialog.getDirectory(), File.separator, fileDialog.getFile());
+        return String.format("%s%s", fileDialog.getDirectory(), fileDialog.getFile());
     }
 
     @Override
