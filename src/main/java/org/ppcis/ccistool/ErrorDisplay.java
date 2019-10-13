117065c36d96a5bb65c7e8fc907aaf31a971d1fc
Brian Ronald <brian@ppcis.org>
2019-10-13 22:58:13 +0100

    Make the columns behave better in the error output.

--- src/main/java/org/ppcis/ccistool/ErrorDisplay.java
+++ src/main/java/org/ppcis/ccistool/ErrorDisplay.java
@@ -3,6 +3,7 @@ package org.ppcis.ccistool;
 import org.ppcis.ccistool.storage.Database;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import java.util.List;
 
 /**
@@ -26,7 +27,17 @@ public class ErrorDisplay {
     private JScrollPane jScrollPane;
 
     public ErrorDisplay(List LEAs) {
+        DefaultTableCellRenderer rightJustifyRenderer = new DefaultTableCellRenderer();
+        rightJustifyRenderer.setHorizontalAlignment(JLabel.RIGHT);
         jTable = new JTable(new Database().errors(LEAs));
+        jTable.getColumnModel().getColumn(0).setMinWidth(110);
+        jTable.getColumnModel().getColumn(0).setMaxWidth(110);
+        jTable.getColumnModel().getColumn(1).setMinWidth(90);
+        jTable.getColumnModel().getColumn(1).setMaxWidth(90);
+        jTable.getColumnModel().getColumn(1).setCellRenderer(rightJustifyRenderer);
+        jTable.getColumnModel().getColumn(3).setMinWidth(60);
+        jTable.getColumnModel().getColumn(3).setMaxWidth(60);
+        jTable.getColumnModel().getColumn(3).setCellRenderer(rightJustifyRenderer);
         jScrollPane = new JScrollPane(jTable);
         jFrame.setContentPane(jScrollPane);
         jFrame.pack();
